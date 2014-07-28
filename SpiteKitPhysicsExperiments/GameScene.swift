4b822d3cf51e6069b70fa43a4b48e1d186ca6c57
Simon Gladman <sgladman@gmail.com>
2014-07-28 21:34:45 +0100

    wip

--- SpiteKitPhysicsExperiments/GameScene.swift
+++ SpiteKitPhysicsExperiments/GameScene.swift
@@ -8,21 +8,37 @@
 
 import SpriteKit
 
+let fieldMask : UInt32 = 0b1;
+let categoryMask: UInt32 = 0b1;
 
 class GameScene: SKScene {
+  
+    let fieldNode: SKFieldNode;
+    //let dragField: SKFieldNode;
     
-    let canvasWidth: UInt32 = 800
-    let canvasHeight: UInt32 = 800
+  
+    init(coder aDecoder: NSCoder!)
+    {
+        //dragField = SKFieldNode.dragField();
+        
+        fieldNode = SKFieldNode.radialGravityField();
+        fieldNode.physicsBody = SKPhysicsBody(circleOfRadius: 80)
+        fieldNode.categoryBitMask = categoryMask;
+        fieldNode.strength = 2.0;
+        super.init(coder: aDecoder)
+    }
+
     
     override func didMoveToView(view: SKView)
     {
-        self.scene.backgroundColor = UIColor.yellowColor();
+        self.scene.backgroundColor = UIColor.darkGrayColor();
         
-        self.physicsWorld.gravity = CGVectorMake(0, -6);
+        self.physicsWorld.gravity = CGVectorMake(0, 0);
         let physicsBody = SKPhysicsBody (edgeLoopFromRect: self.frame);
         self.physicsBody = physicsBody;
+        self.addChild(fieldNode)
         
-        for i in 1...30
+        for i in 1...10
         {
             let shape = SKShapeNode(circleOfRadius: 20);
             shape.strokeColor = UIColor(red: 255, green: 0, blue: 0, alpha: 0.5);
@@ -32,25 +48,28 @@ class GameScene: SKScene {
             self.addChild(shape);
             
             shape.physicsBody = SKPhysicsBody(circleOfRadius: shape.frame.size.width/2)
-            shape.physicsBody.friction = 0.3
-            shape.physicsBody.restitution = 0.9;
-            shape.physicsBody.mass = 0.5
-                      shape.physicsBody.allowsRotation = false
-      
-            let xyzzy : AnyObject = NSKeyedUnarchiver.unarchiveObjectWithFile(NSBundle.mainBundle().pathForResource("MyParticle", ofType: "sks"));
-            var particle:SKEmitterNode = xyzzy as SKEmitterNode;
-            particle.targetNode = shape;
+            shape.physicsBody.friction = 0.7
 
-            particle.particleLifetime = 1.5;
+            shape.physicsBody.restitution = 0.4;
+            shape.physicsBody.mass = 0.75;
+            shape.physicsBody.allowsRotation = false
+            shape.physicsBody.fieldBitMask = fieldMask;
  
-            shape.addChild(particle)
+            let untypedEmitter : AnyObject = NSKeyedUnarchiver.unarchiveObjectWithFile(NSBundle.mainBundle().pathForResource("MyParticle", ofType: "sks"));
+            
+            var emitter:SKEmitterNode = untypedEmitter as SKEmitterNode;
+                       emitter.targetNode = shape;
+ 
+            shape.addChild(emitter)
         }
 
     }
+
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent)
+    override func touchesMoved(touches: NSSet!, withEvent event: UIEvent!)
     {
-  
+        let touch = event.allTouches().anyObject().locationInNode(self);
+        self.fieldNode.position = touch;
     }
    
     override func update(currentTime: CFTimeInterval)
